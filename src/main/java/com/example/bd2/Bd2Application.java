992ed702cbb73c3599281e2853faf43e3c163bc9
Luan Felipe Marmentini <luanf2003@gmail.com>
2024-11-28 12:00:50 -0300

    feat: Add client for componente-produtos

--- src/main/java/com/example/bd2/Bd2Application.java
+++ src/main/java/com/example/bd2/Bd2Application.java
@@ -3,8 +3,10 @@ package com.example.bd2;
 import com.example.bd2.Model.Supplier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Bd2Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/bd2/Service/ProductFeignClient.java
@@ -0,0 +1,16 @@
+package com.example.bd2.Service;
+
+import com.example.bd2.Model.Product;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "product-api", url = "localhost:8081")
+public interface ProductFeignClient {
+
+    @GetMapping("/product/getAll")
+    List<Product> getAll();
+
+}
